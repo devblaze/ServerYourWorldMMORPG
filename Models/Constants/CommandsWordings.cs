aeca3d1279b5314001ac8167bf78cd983d6a1568
Nikolaos Christos Antoniadis <nikosantoniadis94@gmail.com>
2023-10-26 23:34:42 +0300

    Command line logic adjustments and additions

---
+++ Models/Constants/CommandsWordings.cs
@@ -0,0 +1,13 @@
+﻿namespace ServerYourWorldMMORPG.Models.Constants
+{
+    public class CommandsWordings
+    {
+        public const string START = "start";
+        public const string STOP = "stop";
+        public const string GAMESERVER = "game";
+        public const string LOGINSERVER = "login";
+        public const string CLIENTS = "clients";
+        public const string MOCKPACKET = "sendmockpacket";
+        public const string FAKECLIENT = "fakeclient";
+    }
+}
